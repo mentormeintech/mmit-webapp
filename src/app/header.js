ea6aaedd804d2eff254a6028b98a00ed6bcac872
david-aguiyi <davidaguiyi9@gmail.com>
2023-08-10 09:29:32 +0100

    Fix minor deployment bug

--- src/app/header.js
+++ src/app/header.js
@@ -38,7 +38,6 @@ const Header = () => {
 					</div>
 				</div>
 			</div>
-			<p>change</p>
 		</>
 	);
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Header from "./Header/header";
+import Header from "./header";
 
 const Page = () => {
 	return (
