d768cf576acb6fbbbead93ce8087e8b44d478a17
david-aguiyi <davidaguiyi9@gmail.com>
2023-08-12 13:19:11 +0100

    Done with Book a session page

--- src/app/BookASession/page.js
+++ src/app/BookASession/page.js
@@ -8,11 +8,16 @@ const BookASession = () => {
 			<div className="pt-[30px] text-[32px] font-[700] flex-row justify-between items-center ml-[6.4rem] gap-7 inline-flex">
 				Recently Contacted
 			</div>
+			<div className="flex flex-row flex-wrap pt-[36px] items-center justify-center">
 			<ProfileCard />
 			<ProfileCard />
 			<ProfileCard />
 			<ProfileCard />
 			<ProfileCard />
+			<ProfileCard />
+			<ProfileCard />
+			<ProfileCard />
+				</div>
 		</>
 	);
 };
