911304dcab713fea380656fc2ed5cae76555fcef
david-aguiyi <davidaguiyi9@gmail.com>
2023-09-14 20:29:45 +0100

    style: Optimized code to use predefined tailwind values

--- src/pages/findamentor.js
+++ src/pages/findamentor.js
@@ -102,43 +102,38 @@ const FindAMentor = () => {
   return (
     <>
       <Header />
-      <div className='flex flex-row'>
-        <div className='flex flex-row relative z-10 mt-[66px] ml-[100px]'>
+      <div className="ml-24 mt-16 flex flex-row">
+        <div className="relative z-10 w-[90%]">
           <Image
             width={100}
             height={100}
-            src='images/search-icon.svg'
-            className='w-[32.1px] absolute z-40 top-[25px] left-[15px]'
-            alt=''
+            src="images/search-icon.svg"
+            className="absolute left-4 top-6 z-40 w-8"
+            alt=""
           />
           <input
-            type='text'
-            className='w-[1048px] h-[70px] px-[50px] outline-none pl-[60px] border rounded-sm items-center'
-            placeholder='Search by name, role'
+            type="text"
+            className="h-16  w-full items-center rounded-sm border px-12 pl-14 outline-none"
+            placeholder="Search by name, role"
           />
         </div>
       </div>
-      <div className='pt-[30px] flex-row justify-between items-center ml-[6.4rem] gap-7 inline-flex'>
-        <Image src='images/tabler_arrow-up.svg' width={20} height={20} alt='' />
-        <ul className='flex flex-row justify-between ml-[-20px] p-[10px] text-base'>
-          <li className='mr-[30px] text-base cursor-pointer'>All</li>
-          <li className='mr-[30px] cursor-pointer'>Product Design</li>
-          <li className='mr-[30px] cursor-pointer'>Backend Development</li>
-          <li className='mr-[30px] cursor-pointer'>UI/UX</li>
-          <li className='cursor-pointer'>Frontend Development</li>
+      <div className="ml-20 inline-flex flex-row items-center justify-between gap-7 pt-7">
+        <Image src="images/tabler_arrow-up.svg" width={20} height={20} alt="" />
+        <ul className="ml-[-20px] flex flex-row justify-between p-[10px] text-base">
+          <li className="mr-8 cursor-pointer text-base">All</li>
+          <li className="mr-8 cursor-pointer">Product Design</li>
+          <li className="mr-8 cursor-pointer">Backend Development</li>
+          <li className="mr-8 cursor-pointer">UI/UX</li>
+          <li className="cursor-pointer">Frontend Development</li>
         </ul>
       </div>
-      <div className='flex flex-row flex-wrap pt-[36px] items-center justify-center'>
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+      <div className="flex flex-row flex-wrap items-center justify-center">
+        {mentors.map((mentor, index) => (
+          <ProfileCard key={index} mentor={mentor} />
+        ))}
       </div>
-      <div className='mt-[40px]'>
+      <div className="mt-10">
         <Footer />
       </div>
     </>
