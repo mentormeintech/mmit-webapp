a1d1e792bd4cb628762edd809da0e616abe9fc97
david-aguiyi <davidaguiyi9@gmail.com>
2023-09-14 20:26:57 +0100

    style: :art: Optimized code to use predefined tailwind values and react-icons

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,54 +1,47 @@
-import Image from 'next/image';
+import Image from "next/image";
+import { BsFillPersonFill, BsFillClipboard2MinusFill } from "react-icons/bs";
 
-const ProfileCard = () => {
+const ProfileCard = ({ mentor }) => {
   return (
-    <div className="w-[270px] whitespace-nowrap h-[400px] ml-[25px] mt-[30px] border rounded flex flex-col justify-center">
-      <div className="h-[236px]">
+    <div className=" ml-6 mt-28 flex h-[435px] w-72 flex-col justify-center whitespace-nowrap rounded-b-[30rem] rounded-t-full border">
+      <div className="h-56">
         <Image
           className="rounded-t-lg"
           src="/images/profileImage1.png"
-          width={270}
-					height={236}
-					alt='mentor-dp'
+          width={1000}
+          height={236}
+          alt="mentor-dp"
         />
       </div>
-      <div className="mt-[10px]">
-        <p className="text-[20px] ml-[5px] font-semibold">Oluwaseyi Daniels</p>
-        <div className="flex flex-col mt-[10px] ml-[-10px]">
-          <div className="flex text-[12px] flex-row justify-evenly ml-[-20px] ">
-            <Image
-              className="ml-[-15px]"
-              src="/images/draft-icon.svg"
-              width={12}
-							height={12}
-							alt='draft-icon'
-            />
-            <span className="ml-[-40px]">Frontend Developer at ZIP.co</span>
+      <div className="mt-10 flex flex-col items-center">
+        <p className="-ml-20 text-xl font-semibold">{mentor.name}</p>
+        <div className="mt-2 flex flex-col">
+          <div className=" -ml-10 flex flex-row items-center text-sm ">
+            <BsFillClipboard2MinusFill />
+            <span className="ml-2">{mentor.role}</span>
           </div>
-          <div className="text-[12px] flex flex-row justify-evenly ml-[-14px] mt-[10px]">
-            <Image
-              className="w-[11px] ml-[-60px]"
-              src="/images/Vector (12).svg"
-              width={11}
-							height={11}
-							alt='location-icon'
-            />
-            <span className="ml-[-82px]">0 sessions (0 reviews)</span>
+          <div className="-ml-10 mt-2 flex flex-row items-center text-sm">
+            <BsFillPersonFill />
+            <span className="ml-2">
+              {mentor.sessions} sessions ({mentor.reviews} reviews)
+            </span>
           </div>
         </div>
       </div>
-      <div className="text-[12px] p-[10px] h-16 bg-zinc-300 bg-opacity-50 pt-[14px] mt-[1rem] rounded flex flex-row justify-between backdrop:">
+      <div className="backdrop: mt-6 flex h-16 flex-row justify-between rounded bg-zinc-300 bg-opacity-50 p-2 pt-[14px] text-sm">
         <div>
-          <p className="text-neutral-700 text-opacity-70 text-xs font-medium">
+          <p className="text-xs font-medium text-neutral-700 text-opacity-70">
             Experience
           </p>
-          <p className="font-[600]">7 years</p>
+          <p className="font-semibold">{mentor.experience}</p>
         </div>
         <div>
-          <p className="text-neutral-700 text-opacity-70 text-xs font-medium">
+          <p className="text-xs font-medium text-neutral-700 text-opacity-70">
             Avg.Attendance
           </p>
-          <p className="font-[600] text-black text-xs">100%</p>
+          <p className="text-xs font-semibold text-black">
+            {mentor.attendance}
+          </p>
         </div>
       </div>
     </div>
