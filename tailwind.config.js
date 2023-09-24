7a957d4d1805945827d8faf81b0226aba0d3ce26
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-09-24 19:25:38 +0100

    fix nextjs error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-<<<<<<< HEAD
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: { },
-	extend: {
-		backgroundImage: {
-			"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-			"gradient-conic":
-				"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-		},
-	},
-	plugins: [],
-=======
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -40,11 +23,11 @@ module.exports = {
         "hero-bg": "url('/images/hero-bg.jpg')",
       },
       screens: {
-      'xs': '395px',
-      ...defaultTheme.screens,
+        'xs': '395px',
+        ...defaultTheme.screens,
+      }
     },
   },
   extend: {},
   plugins: [],
->>>>>>> master
 };
