2d486e0de23858fa07781a5443c98c754f90ca2c
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-10-14 19:39:20 +0100

    update

--- src/Components/SignupHeader.js
+++ src/Components/SignupHeader.js
@@ -2,31 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 
 const SignupHeader = () => {
-  return (
-<div className="flex flex-row items-center relative">
-					<div className="absolute flex items-center left-20">
-						<Link href="/">
-							<Image
-								src="/images/logo.png"
-								alt="Logo"
-								width={90}
-								height={10}
-								className="cursor-pointer"
-							/>
-						</Link>
-					</div>
+	return (
+		<div className="flex flex-row items-center relative">
+			<div className="absolute flex items-center left-20">
+				<Link href="/">
 					<Image
-						src="/images/background.png"
-						alt="Background"
-						width={2000}
-						height={50}
-						className="h-40"
+						src="/images/logo.png"
+						alt="Logo"
+						width={90}
+						height={10}
+						className="cursor-pointer"
 					/>
-					<h1 className="absolute z-10 text-4xl font-semibold text-white flex text-center left-1/2 transform -translate-x-1/2">
-						Join Us
-					</h1>
-				</div>
-  )
+				</Link>
+			</div>
+			<Image
+				src="/images/background.png"
+				alt="Background"
+				width={2000}
+				height={50}
+				className="h-40"
+			/>
+			<h1 className="absolute z-10 text-4xl font-semibold text-white flex text-center left-1/2 transform -translate-x-1/2">
+				Join Us
+			</h1>
+		</div>
+	)
 }
 
 export default SignupHeader
