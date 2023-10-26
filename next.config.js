2a85cc000e5f065436a3cb405b426e7998b5652a
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-10-26 10:49:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig:nextConfig
+}
