361142723f638225b33c0c7373b21c072dd2662c
david-aguiyi <davidaguiyi9@gmail.com>
2023-11-07 03:47:19 +0100

    style: :lipstick: Added animation to page for athestics

--- src/pages/bookasession.js
+++ src/pages/bookasession.js
@@ -2,15 +2,25 @@ import Header from "../components/Header";
 import ProfileCard from "../components/ProfileCard";
 import Footer from "../components/Footer";
 import mentors from "../components/dummydata";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const Fade = () => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
+};
 
 const BookASession = () => {
   return (
     <div>
       <Header />
-      <div className="inline-flex smd:ml-10 flex-row items-center justify-between gap-7 mt-20 pl-5 text-[29px] font-[700]">
+      <div data-aos="fade-right" className="mt-20 inline-flex flex-row items-center justify-between gap-7 pl-5 text-[29px] font-[700] smd:ml-10">
         Recently Contacted
       </div>
-      <div className="m-auto mt-10 flex w-[20rem] flex-row flex-wrap items-center justify-center gap-10 smd:w-full">
+      <div data-aos="fade-up" className="m-auto mt-10 flex w-[20rem] flex-row flex-wrap items-center justify-center gap-10 smd:w-full">
         {mentors.map((mentor, index) => (
           <ProfileCard
             key={index}
