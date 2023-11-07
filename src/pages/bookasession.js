5dc248405e778aebfc17feb1bcd75ee2b44b9e4d
david-aguiyi <davidaguiyi9@gmail.com>
2023-11-07 03:23:54 +0100

    feat(api): :fire: integrated page with dummy api data

--- src/pages/bookasession.js
+++ src/pages/bookasession.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import ProfileCard from "../components/ProfileCard";
 import Footer from "../components/Footer";
+import mentors from "../components/dummydata";
 
 const BookASession = () => {
   return (
@@ -9,15 +10,19 @@ const BookASession = () => {
       <div className="ml-[6.8rem] inline-flex flex-row items-center justify-between gap-7 pt-[35px] text-[25px] font-[700]">
         Recently Contacted
       </div>
-      <div className="flex flex-row flex-wrap items-center justify-center">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+      <div className="m-auto mt-10 flex w-[20rem] flex-row flex-wrap items-center justify-center gap-10 smd:w-full">
+        {mentors.map((mentor, index) => (
+          <ProfileCard
+            key={index}
+            name={mentor.name}
+            role={mentor.role}
+            sessions={mentor.sessions}
+            reviews={mentor.reviews}
+            experience={mentor.experience}
+            attendance={mentor.attendance}
+            image={mentor.image}
+          />
+        ))}
       </div>
       <div className="mt-20">
         <Footer />
